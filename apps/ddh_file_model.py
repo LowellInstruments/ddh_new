70835ce820d538455f4ffbd3d4b61732039e3dd8
Jeff Grant <jeffery.grant@gmail.com>
2019-09-27 12:26:28 -0400

    adds an FTP file synchronizer and example GUI

---
+++ apps/ddh_file_model.py
@@ -0,0 +1,60 @@
+from PyQt5.QtCore import QAbstractTableModel, QModelIndex
+from PyQt5.QtCore import Qt
+from pathlib import Path
+
+
+FILENAME = 0
+STATUS = 1
+SIZE = 2
+
+
+class FileModel(QAbstractTableModel):
+    def __init__(self, directory, pattern='**/*.lid', parent=None):
+        super().__init__(parent=None)
+        self.directory = directory
+        self.pattern = pattern
+        self.parent = parent
+        self.files = list(Path(self.directory).glob(self.pattern))
+        self._status = ['unknown']*len(self.files)
+
+    def get_status(self, row):
+        return self._status[row]
+
+    def set_status(self, row, status):
+        self._status[row] = status
+        # self.modelReset.emit()  # this works
+        index_left = self.createIndex(0, 0)
+        index_right = self.createIndex(2, self.rowCount()-1)
+        self.dataChanged.emit(index_left, index_right)
+
+    def delete(self, row):
+        self.beginRemoveRows(QModelIndex(), row, row)
+        self.files.pop(row)
+        self._status.pop(row)
+        self.endRemoveRows()
+
+    # pyqt method
+    def rowCount(self, parent=None, *args, **kwargs):
+        return len(self.files)
+
+    # pyqt method
+    def columnCount(self, parent=None, *args, **kwargs):
+        return 3
+
+    # pyqt method
+    def data(self, index, role=None):
+        if role == Qt.DisplayRole:
+            row, col = index.row(), index.column()
+            if col == FILENAME:
+                return self.files[row].name
+            elif col == STATUS:
+                return self._status[row]
+            elif col == SIZE:
+                return '{} KB'.format(self.files[row].stat().st_size // 1024)
+
+    # pyqt method
+    def headerData(self, p_int, orientation, role=None):
+        header = ['File name', 'Status', 'Size']
+        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+            return header[p_int]
+
---
+++ apps/file_uploader_gui.py
@@ -0,0 +1,59 @@
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore
+sys.path.append('../ddh')
+from gui.file_table_ui import Ui_Form
+from apps.ddh_file_model import FileModel
+from apps.ddh_file_uploader import FileUploader
+from pathlib import Path
+
+
+class MainWindow(qtw.QWidget, Ui_Form):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.update_model()
+        self.pushButton_check.clicked.connect(self.sync)
+
+        self.lineEdit_local.editingFinished.connect(self.update_model)
+        self.lineEdit_pattern.editingFinished.connect(self.update_model)
+        self._upload_thread = QtCore.QThread()
+        self.uploader = None
+        self.show()
+
+    def update_model(self):
+        directory = self.lineEdit_local.text()
+        pattern = self.lineEdit_pattern.text()
+        self.model = FileModel(directory, pattern)
+        self.tableView.setModel(self.model)
+
+    def resizeEvent(self, event):
+        width = self.tableView.width()-25
+        self.tableView.setColumnWidth(0, width/2)
+        self.tableView.setColumnWidth(1, width/4)
+        self.tableView.setColumnWidth(2, width/4)
+
+    def sync(self):
+        self._upload_thread.quit()
+        host = ('ftp.lowellinstruments.com',
+                'jeff@lowellinstruments.com',
+                'DDHftp5Woodland')
+        self.uploader = FileUploader(host,
+                                     Path(self.lineEdit_local.text()),
+                                     self.lineEdit_remote.text())
+        self.uploader.moveToThread(self._upload_thread)
+        self.uploader.status.connect(self.status_update)
+        self.uploader.upload_signal.connect(self.status_update)
+        self._upload_thread.start()
+        self.uploader.upload_files(self.model.files)
+
+    def status_update(self, row, n_files, status):
+        self.progressBar.setValue((row+1)/n_files*100)
+        self.model.set_status(row, status)
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    mw = MainWindow()
+    sys.exit(app.exec())
