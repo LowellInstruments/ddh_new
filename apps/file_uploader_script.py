45d46a089141a135bf120a3238ad6b44d59b3099
Jeff Grant <jeffery.grant@gmail.com>
2019-09-30 11:41:55 -0400

    change file_uploader_script to ftp login info from ddh.json

--- apps/file_uploader_script.py
+++ apps/file_uploader_script.py
@@ -1,30 +1,32 @@
+import json
 from apps.ddh_file_uploader import FileUploader
 from pathlib import Path
-
 
 """
 An example of using the file uploader class. See file_uploader_gui for 
 use in a GUI.
 """
 
-
 LOCAL_DIRECTORY = 'C:/Projects/ddh/dl_files'
 
 
 def main():
-        files = list(Path(LOCAL_DIRECTORY).glob('**/*.*'))
-        host = ('ftp.lowellinstruments.com',
-                'jeff@lowellinstruments.com',
-                'DDHftp5Woodland')
+    with open('../ddh.json') as f:
+        config = json.load(f)
 
-        uploader = FileUploader(host, LOCAL_DIRECTORY, '/')
-        uploader.register_observer(update)
-        uploader.upload_files(files)
+    host = (config['ftp_site'],
+            config['ftp_username'],
+            config['ftp_password'])
+
+    files = list(Path(LOCAL_DIRECTORY).glob('**/*.*'))
+    uploader = FileUploader(host, LOCAL_DIRECTORY, '/')
+    uploader.register_observer(update)
+    uploader.upload_files(files)
 
 
 def update(i, n_files, status):
-        print('File {} of {} reports {}'.format(i+1, n_files, status))
+    print('File {} of {} reports {}'.format(i + 1, n_files, status))
 
 
 if __name__ == '__main__':
-        main()
+    main()
