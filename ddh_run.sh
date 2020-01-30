ec082b54e16c0ca9c89e121f968ab07121b04394
kazola <kaz@kaz.com>
2020-01-30 10:31:33 -0500

    fixed code only one instance of application

--- ddh_run.sh
+++ ddh_run.sh
@@ -6,5 +6,14 @@ echo "Running DDH..."
 echo "--------------"
 export XAUTHORITY=/home/pi/.Xauthority
 export DISPLAY=:0
+
+
+# ensure only 1 instance of this bash script
+ps aux | grep ddh_main | grep -v grep
+if [ $? -ne 0 ]; then
+    echo "some DDH already running"
+    echo
+    exit 1
+fi
 cd ~/li/ddh || exit 1
 sudo python3 ddh_main.py
