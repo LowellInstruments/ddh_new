c1cd9f7395bcb365c781018bc8cfadcae64ca595
kazola <kaz@kaz.com>
2020-02-06 11:40:56 -0500

    fixed code only one instance of application

--- ddh_main.py
+++ ddh_main.py
@@ -1,19 +1,9 @@
 import ddh_app as run_app
-import subprocess
-import sys
 
 
 def main():
     run_app.run_app()
 
 
-def ensure_only_one_process():
-    rv = subprocess.run(['pgrep', '-f', 'ddh_main.py'])
-    if rv.returncode == 0:
-        print('python --> some ddh_main.py already running')
-        sys.exit(1)
-    main()
-
-
 if __name__ == "__main__":
-    ensure_only_one_process()
+    main()
