9f53e0c9ae929b8f8bcf020172ffd0f7139a998e
root <root@pop-os.localdomain>
2021-02-18 09:41:02 -0500

    typo

--- tools/script_install_rpi.sh
+++ tools/script_install_rpi.sh
@@ -9,17 +9,21 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 clear
 printf 'Welcome to DDH installer for Raspberry platforms \n'
-printf '================================================ \n'
+printf 'You only need to run this once. When installed, use git pull'
+printf '============================================================ \n\n\n\n'
 
 if [ "$EUID" -ne 0 ]; then printf 'Please run as root'; exit; fi
 
 printf 'Installing Raspberry linux apt dependencies... \n'
+printf '============================================== \n\n\n\n'
 sudo apt-get install libatlas3-base libglib2.0-dev python3-pyqt5 libhdf5-dev python3-dev libgdal-dev
 
 printf 'Installing Raspberry linux python dependencies... \n'
+printf '================================================= \n\n\n\n'
 pip3 install Cartopy==0.18.0
 
 printf 'Cloning DDH source from github... \n'
+printf '================================= \n\n\n\n'
 cd /home/pi
 git clone https://github.com/LowellInstruments/ddh.git
 cd /home/pi/ddh
